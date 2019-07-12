0eacdba1b2291bb0f3e7c9f02cac33cfd91d5ac1
Rynco Maekawa <lynzrand@outlook.com>
2019-07-12 13:54:04 +0800

    Add lexer for operator and identifier

--- src/c0/lexer.rs
+++ src/c0/lexer.rs
@@ -1,9 +1,24 @@
-use crate::c0::ast;
+use crate::c0::ast::{self, Token, TokenVariant};
+use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::iter::{Iterator, Peekable};
 use std::str::{CharIndices, FromStr};
 
 pub struct TokenEntry {}
+
+lazy_static! {
+    static ref __OP_COMBINATION: HashMap<char, Box<Vec<char>>> = {
+        [
+            ('<', Box::new(vec!['='])),
+            ('>', Box::new(vec!['='])),
+            ('=', Box::new(vec!['='])),
+            ('!', Box::new(vec!['='])),
+        ]
+        .iter()
+        .cloned()
+        .collect()
+    };
+}
 
 pub struct Lexer<'a> {
     pub lex_result: Vec<ast::Token<'a>>,
@@ -22,7 +37,7 @@ impl<'a> Lexer<'a> {
         let mut iter: Peekable<CharIndices> = self.src.char_indices().peekable();
         while iter.peek() != None {
             Self::skip_spaces(&mut iter);
-            /// the first character of next token
+            // the first character of next token
             let c = match iter.peek() {
                 Some((_, c)) => c,
                 // spaces may occur at the end of file
@@ -50,9 +65,7 @@ impl<'a> Lexer<'a> {
             end_index += 1;
         }
         let entry = ast::Token {
-            var: ast::TokenVariant::Integer(
-                i64::from_str(&self.src[start_index..end_index]).unwrap(),
-            ),
+            var: TokenVariant::Integer(i64::from_str(&self.src[start_index..end_index]).unwrap()),
             src: &self.src[start_index..end_index],
             pos: start_index,
             end_pos: end_index,
@@ -61,12 +74,96 @@ impl<'a> Lexer<'a> {
     }
 
     fn lex_identifier(&mut self, iter: &mut Peekable<CharIndices>) {
-        // TODO: implement
+        let start_index = iter.peek().expect("This value should be valid").0;
+        let mut end_index = start_index;
+        while iter
+            .peek()
+            .map_or(false, |ch_ind| (*ch_ind).1.is_alphanumeric())
+        {
+            iter.next();
+            end_index += 1;
+        }
+        let variation = match &self.src[start_index..end_index] {
+            "int" => TokenVariant::Int,
+            "void" => TokenVariant::Void,
+            "if" => TokenVariant::If,
+            "else" => TokenVariant::Else,
+            "while" => TokenVariant::While,
+            "return" => TokenVariant::Return,
+            "const" => TokenVariant::Const,
+            _ => TokenVariant::Identifier(&self.src[start_index..end_index]),
+        };
+        let entry = ast::Token {
+            var: variation,
+            src: &self.src[start_index..end_index],
+            pos: start_index,
+            end_pos: end_index,
+        };
+        self.lex_result.push(entry);
     }
 
     fn lex_operator(&mut self, iter: &mut Peekable<CharIndices>) {
-        // TODO: implement
+        let (start_index, first_char) = iter.next().expect("This value should be valid");
+        let mut end_index = start_index + 1;
+        let second_char: Option<char> =
+            __OP_COMBINATION
+                .get(&first_char)
+                .map_or(None, |vec: &Box<Vec<char>>| {
+                    iter.peek().map_or(None, |&(_, ch)| {
+                        if vec[..].contains(&ch) {
+                            Some(ch)
+                        } else {
+                            None
+                        }
+                    })
+                });
+        if second_char.is_some() {
+            iter.next();
+            end_index += 1;
+        }
 
+        let variation = match first_char {
+            '+' => TokenVariant::Plus,
+            '-' => TokenVariant::Minus,
+            '*' => TokenVariant::Multiply,
+            '/' => TokenVariant::Divide,
+            '=' => match second_char {
+                None => TokenVariant::Assign,
+                Some('=') => TokenVariant::Equals,
+                _ => panic!(),
+            },
+            '<' => match second_char {
+                None => TokenVariant::LessThan,
+                Some('=') => TokenVariant::LessOrEqualThan,
+                _ => panic!(),
+            },
+            '>' => match second_char {
+                None => TokenVariant::GreaterThan,
+                Some('=') => TokenVariant::GreaterOrEqualThan,
+                _ => panic!(),
+            },
+            '!' => match second_char {
+                None => TokenVariant::Not,
+                Some('=') => TokenVariant::NotEquals,
+                _ => panic!(),
+            },
+            '(' => TokenVariant::LParenthesis,
+            ')' => TokenVariant::RParenthesis,
+            '{' => TokenVariant::LCurlyBrace,
+            '}' => TokenVariant::RCurlyBrace,
+            _ => panic!(
+                "Unexpected character \'{}\' at {}",
+                &self.src[start_index..end_index],
+                start_index
+            ),
+        };
+        let entry = ast::Token {
+            var: variation,
+            src: &self.src[start_index..end_index],
+            pos: start_index,
+            end_pos: end_index,
+        };
+        self.lex_result.push(entry);
     }
 
     fn skip_spaces(iter: &mut Peekable<CharIndices>) {
