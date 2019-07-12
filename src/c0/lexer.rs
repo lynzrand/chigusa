fc1334b2c87d54a8c1cd4568922b681466a1292b
Rynco Maekawa <lynzrand@outlook.com>
2019-07-12 19:10:52 +0800

    Add iterator for lexer

--- src/c0/lexer.rs
+++ src/c0/lexer.rs
@@ -21,59 +21,76 @@ lazy_static! {
 }
 
 pub struct Lexer<'a> {
-    pub lex_result: Vec<ast::Token<'a>>,
     src: &'a str,
+}
+
+pub struct LexerIterator<'a> {
+    lexer: Lexer<'a>,
+    iter: Option<Peekable<CharIndices<'a>>>,
+}
+
+impl<'a> LexerIterator<'a> {
+    pub fn new(src: Lexer<'a>) -> LexerIterator<'a> {
+        let mut new_iter = LexerIterator {
+            lexer: src,
+            iter: None,
+        };
+        new_iter.iter = Some(new_iter.lexer.src.char_indices().peekable());
+        new_iter
+    }
+}
+
+impl<'a> Iterator for LexerIterator<'a> {
+    type Item = Token<'a>;
+    fn next(&mut self) -> Option<Token<'a>> {
+        self.lexer.get_next_token(self.iter.as_mut().expect(""))
+    }
 }
 
 impl<'a> Lexer<'a> {
     pub fn new(src: &'a str) -> Lexer<'a> {
-        Self {
-            lex_result: vec![],
-            src: src,
-        }
+        Self { src: src }
     }
 
-    pub fn tokenize(&mut self) -> &mut Lexer<'a> {
-        let mut iter: Peekable<CharIndices> = self.src.char_indices().peekable();
-        while iter.peek() != None {
-            Self::skip_spaces(&mut iter);
-            // the first character of next token
-            let c = match iter.peek() {
-                Some((_, c)) => c,
-                // spaces may occur at the end of file
-                None => break,
-            };
+    pub fn get_next_token(&mut self, iter: &mut Peekable<CharIndices>) -> Option<Token<'a>> {
+        Self::skip_spaces(iter);
+        // the first character of next token
+        let c = match iter.peek() {
+            Some((_, c)) => c,
+            // spaces may occur at the end of file
+            None => return None,
+        };
 
-            match c {
-                '0'..='9' => self.lex_number(&mut iter),
-                'a'..='z' | 'A'..='Z' | '_' => self.lex_identifier(&mut iter),
-                '+' | '-' | '*' | '/' | '<' | '>' | '=' | '!' | '(' | ')' | '{' | '}' | ',' => {
-                    self.lex_operator(&mut iter)
-                }
-                // TODO: Add to errors and skip this line
-                _ => panic!("Unexpected character '{}'", c),
-            };
-        }
-        self
+        Some(match c {
+            '0'..='9' => self.lex_number(iter),
+            'a'..='z' | 'A'..='Z' | '_' => self.lex_identifier(iter),
+            '+' | '-' | '*' | '/' | '<' | '>' | '=' | '!' | '(' | ')' | '{' | '}' | ',' => {
+                self.lex_operator(iter)
+            }
+            // TODO: Add to errors and skip this line
+            _ => panic!("Unexpected character '{}'", c),
+        })
     }
 
-    fn lex_number(&mut self, iter: &mut Peekable<CharIndices>) {
+    fn lex_number(&mut self, iter: &mut Peekable<CharIndices>) -> Token<'a> {
         let start_index = iter.peek().expect("This value should be valid").0;
         let mut end_index = start_index;
         while iter.peek().map_or(false, |ch_ind| (*ch_ind).1.is_digit(10)) {
             iter.next();
             end_index += 1;
         }
-        let entry = ast::Token {
-            var: TokenVariant::Integer(i64::from_str(&self.src[start_index..end_index]).unwrap()),
+
+        Token {
+            var: TokenVariant::IntegerLiteral(
+                i64::from_str(&self.src[start_index..end_index]).unwrap(),
+            ),
             src: &self.src[start_index..end_index],
             pos: start_index,
             end_pos: end_index,
-        };
-        self.lex_result.push(entry);
+        }
     }
 
-    fn lex_identifier(&mut self, iter: &mut Peekable<CharIndices>) {
+    fn lex_identifier(&mut self, iter: &mut Peekable<CharIndices>) -> Token<'a> {
         let start_index = iter.peek().expect("This value should be valid").0;
         let mut end_index = start_index;
         while iter
@@ -93,16 +110,16 @@ impl<'a> Lexer<'a> {
             "const" => TokenVariant::Const,
             _ => TokenVariant::Identifier(&self.src[start_index..end_index]),
         };
-        let entry = ast::Token {
+
+        Token {
             var: variation,
             src: &self.src[start_index..end_index],
             pos: start_index,
             end_pos: end_index,
-        };
-        self.lex_result.push(entry);
+        }
     }
 
-    fn lex_operator(&mut self, iter: &mut Peekable<CharIndices>) {
+    fn lex_operator(&mut self, iter: &mut Peekable<CharIndices>) -> Token<'a> {
         let (start_index, first_char) = iter.next().expect("This value should be valid");
         let mut end_index = start_index + 1;
         let second_char: Option<char> =
@@ -157,13 +174,13 @@ impl<'a> Lexer<'a> {
                 start_index
             ),
         };
-        let entry = ast::Token {
+
+        Token {
             var: variation,
             src: &self.src[start_index..end_index],
             pos: start_index,
             end_pos: end_index,
-        };
-        self.lex_result.push(entry);
+        }
     }
 
     fn skip_spaces(iter: &mut Peekable<CharIndices>) {
@@ -174,11 +191,12 @@ impl<'a> Lexer<'a> {
             iter.next();
         }
     }
+}
 
-    // fn get_next_token(& self, cur_ptr: usize)->(ast::Token,  usize) {
-    // }
-
-    // fn get_identifier_token(& self, cur_ptr: usize)->(ast::Token, usize){
-
-    // }
+impl<'a> IntoIterator for Lexer<'a> {
+    type Item = Token<'a>;
+    type IntoIter = LexerIterator<'a>;
+    fn into_iter(self) -> Self::IntoIter {
+        LexerIterator::new(self)
+    }
 }
