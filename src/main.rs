206e4391fcd90a3fec0a46c8053fed279e2be01e
Rynco Maekawa <lynzrand@outlook.com>
2019-07-15 11:26:24 +0800

    Write small test program in main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use chigusa::c0::ast;
+use chigusa::c0::{ast, lexer};
 use crossterm;
 use crossterm::{cursor, Color, Colored};
 
+const __INPUT_CODE: &'static str = r"
+int x, y;
+const k = 3;
+int main(){ printf(x); }
+";
+
 fn main() {
-    println!(
-        "{}Hello, world!",
-        crossterm::Colored::Fg(crossterm::Color::Red)
-    );
+    let vec: Vec<ast::Token> = lexer::Lexer::new(__INPUT_CODE).into_iter().collect();
+    print!("{:#?}", vec);
 }
