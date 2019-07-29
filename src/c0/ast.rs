d662dffe3fe279d2b52eab5b51fd692f19ba069c
Rynco Maekawa <lynzrand@outlook.com>
2019-07-29 16:48:43 +0800

    Add scopes for token entries

--- src/c0/ast.rs
+++ src/c0/ast.rs
@@ -4,26 +4,73 @@
 */
 
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::iter::Iterator;
+use std::ops::{Deref, DerefMut};
 use std::rc::Rc;
 
-type Ptr<T> = Rc<RefCell<T>>;
+pub struct Ptr<T>(Rc<RefCell<T>>);
+
+impl<T> Ptr<T> {
+    pub fn new(val: T) -> Ptr<T> {
+        Ptr(Rc::new(RefCell::new(val)))
+    }
+}
+
+impl<T> Clone for Ptr<T> {
+    fn clone(&self) -> Self {
+        Ptr(self.0.clone())
+    }
+}
 
 pub struct Program {
-    pub decl: Vec<VarDecalaration>,
-    pub fns: Vec<FnDeclaration>,
+    pub scope: Ptr<Scope>,
+    pub decl: Vec<Ptr<VarDecalaration>>,
+    pub fns: Vec<Ptr<FnDeclaration>>,
 }
 
 pub struct FnDeclaration {
     pub ident: Identifier,
     pub return_typ: Identifier,
-    pub params: Vec<VarDecalaration>,
+    pub params: Vec<Ptr<VarDecalaration>>,
     pub body: Block,
 }
 
 pub struct Block {
-    pub decl: Vec<VarDecalaration>,
+    pub scope: Ptr<Scope>,
+    pub decl: Vec<Ptr<VarDecalaration>>,
     pub stmt: Vec<Statement>,
+}
+
+pub enum TokenEntry {
+    Variable {
+        is_const: bool,
+        var_type: String,
+        decl: Ptr<VarDecalaration>,
+    },
+    Type {
+        is_primitive: bool,
+        occupy_bytes: usize,
+    },
+    Function {
+        return_type: String,
+        params_type: Vec<String>,
+        decl: Ptr<FnDeclaration>,
+    },
+}
+
+pub struct Scope {
+    pub parent: Option<Ptr<Scope>>,
+    pub token_table: HashMap<String, TokenEntry>,
+}
+
+impl Scope {
+    pub fn new(parent: Option<Ptr<Scope>>) -> Scope {
+        Scope {
+            parent,
+            token_table: HashMap::new(),
+        }
+    }
 }
 
 pub enum Statement {
--- src/c0/parser.rs
+++ src/c0/parser.rs
@@ -1,3 +1,4 @@
+use crate::c0::ast::*;
 use crate::c0::lexer::*;
 use lazy_static::lazy_static;
 use std::collections::*;
@@ -15,83 +16,41 @@ impl<'a> IntoParser<'a> for dyn Iterator<Item = Token<'a>> {
 
 pub struct Parser<'a> {
     lexer: Box<dyn Iterator<Item = Token<'a>>>,
-    stack: VecDeque<AstNode<'a>>,
 }
 
 impl<'a> Parser<'a> {
     pub fn new(lexer: Box<dyn Iterator<Item = Token<'a>>>) -> Parser<'a> {
         Parser {
             lexer,
-            stack: VecDeque::new(),
+            // stack: VecDeque::new(),
         }
     }
 
-    pub fn parse(&mut self) -> AstNode<'a> {
+    pub fn parse(&mut self) -> Result<Program, ParseErrors> {
+        self.parse_program()
+    }
+
+    fn parse_program(&mut self) -> Result<Program, ParseErrors> {
+        let mut scope = Ptr::new(Scope::new(None));
+        let decl = self.parse_decls(scope.clone())?;
+        let fns = self.parse_fns(scope.clone())?;
+        Ok(Program {
+            scope: scope.clone(),
+            fns,
+            decl,
+        })
+        // unimplemented!()
+    }
+
+    fn parse_decls(&mut self, scope: Ptr<Scope>) -> Result<Vec<Ptr<VarDecalaration>>, ParseErrors> {
         unimplemented!()
     }
+
+    fn parse_fns(&mut self, scope: Ptr<Scope>) -> Result<Vec<Ptr<FnDeclaration>>, ParseErrors> {
+        unimplemented!()
+    }
+
+    // fn parse_decl(&mut self, scope: Ptr<Scope>)->Result
 }
 
-pub enum TokenEntry<'a> {
-    Variable {
-        is_const: bool,
-        var_type: &'a TokenEntry<'a>,
-    },
-    Type {},
-    Function {
-        return_type: &'a TokenEntry<'a>,
-        params_type: Vec<&'a TokenEntry<'a>>,
-    },
-}
-
-pub struct Scope<'a> {
-    pub token_table: HashMap<&'a str, TokenEntry<'a>>,
-}
-
-type ClassRef<T> = Rc<RefCell<T>>;
-
-#[derive(Debug)]
-pub enum AstNodeVariation<'a> {
-    ConstDeclaration {
-        children: Rc<RefCell<Vec<AstNode<'a>>>>,
-    },
-    VariableDeclaration {
-        children: Rc<RefCell<Vec<AstNode<'a>>>>,
-    },
-    Identifier {
-        ident: &'a str,
-    },
-    BinaryOperation {
-        opcode: char,
-        is_left_combine: bool,
-        lhs: Rc<RefCell<AstNode<'a>>>,
-        rhs: Rc<RefCell<AstNode<'a>>>,
-    },
-    UnaryOperation {
-        opcode: char,
-        is_left_combine: bool,
-        side: Rc<RefCell<AstNode<'a>>>,
-    },
-    IfStatement {
-        checked_expression: Rc<RefCell<AstNode<'a>>>,
-        has_else: bool,
-        if_body: Rc<RefCell<AstNode<'a>>>,
-        else_body: Option<Rc<RefCell<AstNode<'a>>>>,
-    },
-    WhileStatement {
-        checked_expression: Rc<RefCell<AstNode<'a>>>,
-        body: Rc<RefCell<AstNode<'a>>>,
-    },
-    Block {
-        children: Rc<RefCell<Vec<AstNode<'a>>>>,
-    },
-    Document {
-        children: Rc<RefCell<Vec<AstNode<'a>>>>,
-    },
-}
-
-#[derive(Debug)]
-pub struct AstNode<'a> {
-    pub var: AstNodeVariation<'a>,
-    pub start_pos: usize,
-    pub end_pos: usize,
-}
+pub enum ParseErrors {}
