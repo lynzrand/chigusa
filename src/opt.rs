fc84c9bf348ade894eaf58ec2af7dd3178dda0bb
Rynco Maekawa <lynzrand@outlook.com>
2019-12-24 19:54:45 +0800

    Edit cli to match required behavior

--- src/opt.rs
+++ src/opt.rs
@@ -18,14 +18,22 @@ fn parse_verbosity(input: &str) -> Result<log::LevelFilter, &'static str> {
 }
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "chigusa")]
+#[structopt(
+    name = "chigusa",
+    about = r"
+A compiler that parses c0 grammar and compiles it into o0 binary format.
+
+C0: https://github.com/BUAA-SE-Compiling/c0-handbook
+O0: https://github.com/BUAA-SE-Compiling/c0-vm-standards
+"
+)]
 pub struct ParserConfig {
     /// Input file. Defaults to stdin if no file were supplied.
     #[structopt(name = "file", parse(from_os_str))]
     pub input_file: Option<PathBuf>,
 
     /// Output file.
-    #[structopt(short, long = "out", default_value = "a.out", parse(from_os_str))]
+    #[structopt(short, long = "out", default_value = "out", parse(from_os_str))]
     pub output_file: PathBuf,
 
     /// Verbossity. Allowed values are: debug, trace, info, warn, error, off.
@@ -45,8 +53,18 @@ pub struct ParserConfig {
     /// Emit result explanation:
     /// - Token: Direct result from lexer (tokenizer)
     /// - AST: Abstract Syntax Tree, direct result from parser (analyzer)
+    /// - s0: C0 assembly file
+    /// - o0: C0 binary file
     #[structopt(long, default_value = "o0", parse(try_from_str = EmitOption::parse))]
     pub emit: EmitOption,
+
+    /// Emit C0 assembly file, same as `--emit s0`
+    #[structopt(short = "s", long = "s0")]
+    pub output_assembly: bool,
+
+    /// Emit C0 binary file, same as `--emit o0`
+    #[structopt(short = "c", long = "o0")]
+    pub output_binary: bool,
 }
 
 #[derive(Debug, Eq, PartialEq)]
